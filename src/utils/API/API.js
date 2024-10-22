1c7d262172a0852e791c76fb9d20dcc8c2d213d5
vimarupon@hotmail.com <vimarupon@gmail.com>
2024-10-22 11:30:55 +0200

    new changes

--- src/utils/API/API.js
+++ src/utils/API/API.js
@@ -1,4 +1,4 @@
-const URL = "https://proyecto-10-full-stack.vercel.app/api/v1/";
+const URL = "https://proyecto-10-full-stack.vercel.app/api/v1";
 
 export const API = async ({
   endpoint,
@@ -11,12 +11,15 @@ export const API = async ({
     Authorization: `Bearer ${token}`,
   };
 
-  isJSON ? (headers["Content-Type"] = "application/json") : null;
+  if (isJSON) {
+    headers["Content-Type"] = "application/json";
+  }
 
   const res = await fetch(URL + endpoint, {
     body: isJSON ? JSON.stringify(body) : body,
     method,
     headers,
+    credentials: "include",
   });
 
   const response = await res.json();
